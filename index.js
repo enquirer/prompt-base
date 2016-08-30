f80fb5242783960161679000952dde3e8eb7cbc4
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-30 04:44:22 -0400

    adds basic `input` prompt support
    for easier testing/test-driving. also adds code comments

--- index.js
+++ index.js
@@ -3,21 +3,36 @@
 var log = require('log-utils');
 var define = require('define-property');
 var Emitter = require('component-emitter');
-var Question = require('enquirer-question');
-var isNumber = require('is-number');
-var utils = require('readline-utils');
+var Question = require('prompt-question');
 var UI = require('readline-ui');
 
 /**
- * Create a new Prompt
+ * Create a new Prompt with the given `question` object, `answers` and optional instance
+ * of [readline-ui][].
+ *
+ * ```js
+ * var prompt = new Prompt({
+ *   name: 'color',
+ *   message: 'What is your favorite color?'
+ * });
+ *
+ * prompt.ask(function(answer) {
+ *   console.log(answer);
+ *   //=> 'blue'
+ * });
+ * ```
+ * @param {Object} `question` Plain object or instance of [prompt-question][].
+ * @param {Object} `answers` Optionally pass an answers object from a prompt manager (like [enquirer][]).
+ * @param {Object} `ui` Optionally pass an instance of [readline-ui][]. If not passed, an instance is created for you.
+ * @api public
  */
 
 function Prompt(question, answers, ui) {
-  this.ui = ui;
   this.question = new Question(question);
   this.answers = answers || {};
   this.status = 'pending';
-  this.active = true;
+  this.session = true;
+  this.ui = ui;
 
   // Check to make sure prompt requirements are there
   if (!isString(this.question.message)) {
@@ -28,14 +43,14 @@ function Prompt(question, answers, ui) {
   }
 
   // Normalize choices
-  if (Array.isArray(this.question.choices)) {
-    this.question.addChoices(this.question.choices, answers);
+  if (this.question.choices) {
+    this.question.addChoices(this.question.choices, this.answers);
   }
 
   define(this, 'options', this.question);
   if (typeof this.ui === 'undefined') {
     this.ui = new UI(this.options);
-    this.active = false;
+    this.session = false;
   }
 
   this.rl = this.ui.rl;
@@ -65,7 +80,7 @@ Prompt.prototype.when = function() {
 
 Prompt.prototype.validate = function(val) {
   if (typeof this.question.validate === 'function') {
-    return this.question.validate(val);
+    return this.question.validate.apply(this, arguments);
   }
   return true;
 };
@@ -96,31 +111,29 @@ Prompt.prototype.transform = function(val) {
  * Default `ask` method, overridden in custom prompts.
  */
 
-Prompt.prototype.ask = function(next) {
-  next();
+Prompt.prototype.ask = function(callback) {
+  this.callback = callback;
+  this.ui.on('keypress', this.render.bind(this));
+  this.ui.once('line', this.onSubmit.bind(this));
+  this.render();
 };
 
 /**
- * Used if `when` returns false
- */
-
-Prompt.prototype.noop = function(next) {
-  next();
-};
-
-/**
- * Initialize prompt session and resolve answers.
+ * Initialize a prompt and resolve answers. If `question.when` returns false,
+ * the prompt will be skipped.
+ *
+ * @param {Object} `answers`
  * @return {Promise}
+ * @api public
  */
 
 Prompt.prototype.run = function(answers) {
-  var self = this;
+  var name = this.question.name;
+  var when = this.when(answers);
+  var ask = when ? this.ask.bind(this) : this.noop;
+  answers = answers || this.answers;
 
   return new Promise(function(resolve) {
-    var name = self.question.name;
-    var when = self.when(answers);
-    var ask = when ? self.ask.bind(self) : self.noop;
-
     ask(function(value) {
       if (typeof value !== 'undefined') {
         answers[name] = value;
@@ -131,17 +144,95 @@ Prompt.prototype.run = function(answers) {
 };
 
 /**
- * Generate the prompt question string
- * @return {String} prompt question string
+ * Render the current prompt input. This can be replaced by custom prompts.
+ *
+ * ```js
+ * prompt.ui.on('keypress', prompt.render.bind(prompt));
+ * ```
+ * @api public
+ */
+
+Prompt.prototype.render = function() {
+  this.ui.render(this.message + this.rl.line);
+};
+
+/**
+ * When the answer is submitted (user presses `enter` key), re-render
+ * and pass answer to callback. This can be replaced by custom prompts.
+ * @param {Object} `input`
+ */
+
+Prompt.prototype.onSubmit = function(input) {
+  this.status = 'answered';
+  this.answer = input;
+  this.emit('answer', input);
+  this.submitAnswer();
+};
+
+/**
+ * Re-render and pass the final answer to the callback. This can be replaced
+ * by custom prompts, but it probably won't need to be.
+ */
+
+Prompt.prototype.submitAnswer = function() {
+  this.render();
+  this.ui.write();
+  if (!this.session) this.close();
+  this.callback(this.answer);
+};
+
+/**
+ * Returns a formatted prompt message.
+ * @return {String}
+ * @api public
  */
 
 Prompt.prototype.format = function(msg) {
-  var message = log.green('?') + ' ' + log.bold(msg) + ' ';
+  var message = this.prefix + log.bold(msg) + ' ';
   if (this.question.hasDefault && this.status !== 'answered') {
     message += log.dim('(' + this.question.default + ') ');
   }
   return message;
 };
+
+/**
+ * Used if `when` returns false
+ */
+
+Prompt.prototype.noop = noop;
+
+/**
+ * Getter that true if the prompt is in a session with multiple questions. This value
+ * is set in implementations by a prompt manager, like [enquirer].
+ *
+ * @name .session
+ * @return {Boolean} True if a prompt session is active.
+ * @api public
+ */
+
+Object.defineProperty(Prompt.prototype, 'session', {
+  set: function(val) {
+    define(this, '_session', val);
+  },
+  get: function() {
+    if (typeof this._session === 'boolean') {
+      return this._session;
+    }
+    // enquirer 0.1.0 support. will be removed in future version
+    if (typeof this.active === 'boolean') {
+      return this.active;
+    }
+    return !!this._session;
+  }
+});
+
+/**
+ * Getter for getting the choices array from the question.
+ *
+ * @name .choices
+ * @return {Object} Choices object
+ * @api public
+ */
 
 Object.defineProperty(Prompt.prototype, 'choices', {
   set: function(val) {
@@ -151,6 +242,14 @@ Object.defineProperty(Prompt.prototype, 'choices', {
     return this.question.choices;
   }
 });
+
+/**
+ * Getter that returns `question.message` after passing it to [format](#format).
+ *
+ * @name .message
+ * @return {String} A formatted prompt message.
+ * @api public
+ */
 
 Object.defineProperty(Prompt.prototype, 'message', {
   set: function() {
@@ -162,8 +261,30 @@ Object.defineProperty(Prompt.prototype, 'message', {
 });
 
 /**
+ * Getter that returns the prefix to use before `question.message`. The
+ * default value is a green `?`.
+ *
+ * @name .prefix
+ * @return {String} The formatted prefix.
+ * @api public
+ */
+
+Object.defineProperty(Prompt.prototype, 'prefix', {
+  set: function() {
+    throw new Error('.prefix is a getter and cannot be defined');
+  },
+  get: function() {
+    return this.question.prefix || (log.cyan('?') + ' ');
+  }
+});
+
+/**
  * Utils
  */
+
+function noop(next) {
+  next();
+}
 
 function isString(val) {
   return val && typeof val === 'string';
