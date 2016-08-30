3f352cf270a27830b534fba6fd7c9ca26a134709
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-30 06:47:52 -0400

    move examples

--- examples/prompt.js
+++ examples/prompt.js
@@ -1,4 +1,4 @@
-var Prompt = require('./');
+var Prompt = require('..');
 var prompt = new Prompt({
   name: 'color',
   message: 'What is your favorite color?'
@@ -8,7 +8,4 @@ prompt.ask(function(answer) {
   console.log(answer);
 });
 
-// prompt.run()
-//   .then(function(answers) {
-//     console.log(answers);
-//   });
+
