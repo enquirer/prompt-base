89fec3e9859d39ae505f725c922d5af5dcd3a038
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-01 17:08:26 -0400

    examples for `options.validate`

---
+++ examples/ask.nested.validate.js
@@ -0,0 +1,20 @@
+var Prompt = require('..');
+var prompt = new Prompt({
+  name: 'first',
+  message: 'What is your name?',
+  validate: function(str) {
+    if (!/^[a-z]+$/i.test(str)) {
+      return 'invalid value';
+    }
+    return true;
+  }
+});
+
+prompt.ask(function(answers) {
+  console.log(answers);
+
+  prompt.run()
+    .then(function(answers) {
+      console.log(answers);
+    });
+});
